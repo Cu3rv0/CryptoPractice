7740d76833209f01d27c1a987e2d597713f40bf1
Cu3rv0 <pablomansanet@gmail.com>
2015-03-15 11:42:37 +0000

    Small modifications

--- tests/tests.Converter.cpp
+++ tests/tests.Converter.cpp
@@ -31,7 +31,14 @@ TEST_F(ConversionTests, converter_reads_and_outputs_right_hex_value)
    ASSERT_EQ(hexString,m_converter->GetValueHex());
 }
 
-TEST_F(ConversionTests, converter_outputs_right_hex_value)
+TEST_F(ConversionTests, converter_reads_and_outputs_right_b64_value)
 {
-   //
+   // Given
+   const string b64String = "SSdt";
+   const vector<unsigned char> expectedData = (0b01001001,0b00100111,0b01101101);
+
+   //When
+   //Then
+   ASSERT_EQ(expectedData,m_converter->GetValueBinary());
+   ASSERT_EQ(hexString,m_converter->GetValueHex());
 }
